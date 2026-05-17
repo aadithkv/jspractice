15d4359e8f66c830d4618c9acdffcf70cdc75478
Aadith <aadithkvijay.vijay94@gmail.com>
2026-05-17 23:37:20 +0530

    add demo change

--- firstTs.ts
+++ firstTs.ts
@@ -3,8 +3,9 @@ console.log("hello")
 let na:string ='Aadith'
 let age:number=12;
 let isFail:boolean=false;
-age=a;
+//age='a';
 
 console.log(na)
 console.log(age)
 console.log(isFail)
+console.log(3)
